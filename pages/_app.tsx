bc489dec118bec8224afbb125915c17e88e1ea22
La Quoc Anh <anh.lq@trungthuy.vn>
2022-01-29 10:34:06 +0700

    update web title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from "next/app"
 import { ChakraProvider } from "@chakra-ui/react"
 import { QueryClient, QueryClientProvider } from "react-query"
+import Head from "next/head"
 
 const qc = new QueryClient()
 
@@ -8,6 +9,9 @@ function MyApp({ Component, pageProps }: AppProps) {
     return (
         <ChakraProvider>
             <QueryClientProvider client={qc}>
+                <Head>
+                    <title>{`Merkle Root Generator & Verifier`}</title>
+                </Head>
                 <Component {...pageProps} />
             </QueryClientProvider>
         </ChakraProvider>
